2a07e21a62016e50ef8ac3598365578a5f9f3a0c
atwoodkevin <32721793+atwoodkevin@users.noreply.github.com>
2018-10-22 10:10:37 -0700

    update

--- ssh.sh
+++ ssh.sh
@@ -1,4 +1,4 @@
 read -p "Enter ip address: " IP_ADDRESS
 ssh katwood@$IP_ADDRESS "mkdir -p ~/.ssh && chmod 700 ~/.ssh"
 cat ~/.ssh/id_rsa.pub | ssh katwood@$IP_ADDRESS "cat >> ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys"
-
+cat ~/.ssh/id_rsa | ssh 192.168.7.236 "cat >> ~/.ssh/id_rsa && chmod 600 ~/.ssh/id_rsa"
